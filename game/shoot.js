6d642efc319830f0111cefff693acfa6a3e53034
caro <ryubedraw@gmail.com>
2022-09-28 19:28:09 +0200

    3 & 4 step

--- game/shoot.js
+++ game/shoot.js
@@ -82,7 +82,8 @@ function player_falling()
 
     for (var i = 0; i < length; i++) {
         element = noGround[i];
-
+        if (element == undefined)
+            continue;
         var tileX = (element[0]) | 0;
         var tileY = (element[1]) | 0;
         var mtileX = (element[0] + sizeOfTileX) | 0;
